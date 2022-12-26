e3c62013b55fefc94b5408182fb885860d4f81e7
Ali Cangül <alicangul@Ali-MacBook-Pro.home>
2022-12-26 15:59:44 +0300

    design edit

---
+++ javascript.js
@@ -0,0 +1,4 @@
+let user = document.getElementById("username");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let repassword = document.getElementById("repassword");
