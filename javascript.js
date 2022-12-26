02174117e8ec9e58a30fd3a675435b590fb410bd
Ali Cangül <alicangul@Ali-MacBook-Pro.home>
2022-12-26 16:42:22 +0300

    form validate added

--- javascript.js
+++ javascript.js
@@ -1,4 +1,30 @@
-let user = document.getElementById("username");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-let repassword = document.getElementById("repassword");
+const form = document.getElementById("form");
+const user = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const repassword = document.getElementById("repassword");
+
+function error(input, message) {
+  input.className = "form-control is-invalid";
+  const div = input.nextElementSibling;
+  div.innerText = message;
+  div.className = "invalid-feedback";
+}
+function success(input) {
+  input.className = "form-control is-valid";
+}
+function checkedRequired(inputs) {
+  inputs.forEach(function (input) {
+    if (input.value === "") {
+      error(input, `${input.id} boş bırakılamaz`);
+    } else {
+      success(input);
+    }
+  });
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  checkedRequired([user, email, password, repassword]);
+});
